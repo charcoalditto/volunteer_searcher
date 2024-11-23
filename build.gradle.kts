803462b090aff0fb2077c02ce89c9ba23ec33dd2
sol <charcoalditto@gmail.com>
2024-11-23 22:26:58 +0900

    feat: swagger 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
