c16f9ba86f8d9ba24e0447390b666c7ba41df039
sol <charcoalditto@gmail.com>
2024-12-02 01:17:02 +0900

    imp: 로그 색깔 추가 >_<

--- src/main/kotlin/sol/volunteer_searcher/util/MyUtils.kt
+++ src/main/kotlin/sol/volunteer_searcher/util/MyUtils.kt
@@ -4,6 +4,39 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 
-inline fun <reified T> T.logger(): Logger {
-    return LoggerFactory.getLogger("@@-" + T::class.simpleName)
+inline fun <reified T> T.logger(): ColorfulLogger {
+    return ColorfulLogger(LoggerFactory.getLogger("@@-" + T::class.simpleName))
+}
+
+class ColorfulLogger(private val logger: Logger) {
+    companion object {
+        private const val BLACK = "\u001B[30m"
+        private const val RED = "\u001B[31m"
+        private const val GREEN = "\u001B[32m"
+        private const val YELLOW = "\u001B[33m"
+        private const val BLUE = "\u001B[34m"
+        private const val MAGENTA = "\u001B[35m"
+        private const val CYAN = "\u001B[36m"
+        private const val WHITE = "\u001B[37m"
+        private const val RESET = "\u001B[0m"
+    }
+
+    private val color = GREEN
+
+
+    fun info(message: String) {
+        logger.info("$color$message$RESET")
+    }
+
+    fun debug(message: String) {
+        logger.debug("$color$message$RESET")
+    }
+
+    fun warn(message: String) {
+        logger.warn("$color$message$RESET")
+    }
+
+    fun error(message: String) {
+        logger.error("$color$message$RESET")
+    }
 }
